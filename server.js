268219f17d69d3922cb99da369f268d1299d9750
awallathome <adam.wallis@me.com>
2017-11-14 18:10:29 -0500

    final commit before deployment

--- server.js
+++ server.js
@@ -57,24 +57,24 @@ app.get("/scrape", function(req, res) {
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
-    //   result.title = $(this)
-    //     .children("a")
-    //     .text();
-    //   result.link = $(this)
-    //     .children("a")
-    //     .attr("href");
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
 
-    //   // Create a new Article using the `result` object built from scraping
-    //   db.Article
-    //     .create(result)
-    //     .then(function(dbArticle) {
-    //       // If we were able to successfully scrape and save an Article, send a message to the client
-    //       res.send("Scrape Complete");
-    //     })
-    //     .catch(function(err) {
-    //       // If an error occurred, send it to the client
-    //       res.json(err);
-    //     });
+      // Create a new Article using the `result` object built from scraping
+      db.Article
+        .create(result)
+        .then(function(dbArticle) {
+          // If we were able to successfully scrape and save an Article, send a message to the client
+          res.send("Scrape Complete");
+        })
+        .catch(function(err) {
+          // If an error occurred, send it to the client
+          res.json(err);
+        });
     });
   });
 });
